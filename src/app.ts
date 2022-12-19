75c1556eeab311ef37be4e49520c313fc9a3d0ca
Andre Perregil <andre_perregil@outlook.com>
2022-12-19 14:44:09 +0000

    feat: App middleware to handle errors inplemented

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import "express-async-errors";
 import express from "express";
 //import { appRoutes } from "./routes";
 
+import handleErrorMiddleware from "./migrations/errors/handleError.middleware";
+
 const app = express();
 app.use(express.json());
 //appRoutes(app);
+app.use(handleErrorMiddleware);
 
 export default app;
