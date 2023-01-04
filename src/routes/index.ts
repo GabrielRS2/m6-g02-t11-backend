ed52335d4d4e29270a38d980f225e25a2848db2f
DavidBassouto <davidbassouto@hotmail.com>
2023-01-04 16:10:48 -0300

    feat: users login, addition packages bcryptjs and jsonwebtoken and his respective @types

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Express } from "express";
 
 import { productsRoutes } from "./products.routes";
+import { sessionRoutes } from "./session.routes";
 import { usersRoutes } from "./users.routes";
 
 export const appRoutes = (app: Express) => {
   app.use("/products", productsRoutes());
   app.use("/users", usersRoutes());
+  app.use("/login", sessionRoutes());
 };
