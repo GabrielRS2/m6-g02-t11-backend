72a4dcadf67bacb46701d23c8ab327e94bbd94cb
Andre Perregil <andre_perregil@outlook.com>
2023-01-06 11:51:52 +0000

    feat:Route that lists all comments from a product

---
+++ src/services/comments/commentsListFromProduct.service.ts
@@ -0,0 +1,24 @@
+import AppDataSource from "../../data-source";
+import { AppError } from "../../errors/AppError";
+
+import { Product } from "../../entities/products.entity";
+import { Comment } from "../../entities/comments.entity";
+
+export const commentsListFromProductService = async (
+  id: string
+): Promise<Comment[]> => {
+  const productRepo = AppDataSource.getRepository(Product);
+  const commentRepo = AppDataSource.getRepository(Comment);
+
+  const product = await productRepo.findOneBy({ id });
+  if (!product) {
+    throw new AppError(404, "Product not found<param>");
+  }
+
+  const comments = await commentRepo.find({
+    relations: { product: true },
+    where: { product: product },
+  });
+
+  return comments;
+};
