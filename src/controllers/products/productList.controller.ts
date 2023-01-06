5f696c66f4f5459e4b71ac4db8000b8db54c0ae9
Andre Perregil <andre_perregil@outlook.com>
2023-01-06 13:39:20 +0000

    fix: now product route show all picture and the user(seller)

--- src/controllers/products/productList.controller.ts
+++ src/controllers/products/productList.controller.ts
@@ -1,3 +1,4 @@
+import { instanceToPlain } from "class-transformer";
 import { Request, Response } from "express";
 
 import { listProductService } from "../../services/products/productList.service";
@@ -6,7 +7,8 @@ export const productListController = async (req: Request, res: Response) => {
   const { id } = req.params;
   const product = await listProductService(id);
 
-  return res
-    .status(200)
-    .json({ message: "Product retrieved sucessfully", product: product });
+  return res.status(200).json({
+    message: "Product retrieved sucessfully",
+    product: instanceToPlain(product),
+  });
 };
